3a1aede8a9128862601bc9f406be6f47bcd2415f
Leo.Kuo <et282523@hotmail.com>
2019-05-27 11:56:34 +0800

    測用 float 做 auto layout

---
+++ float-auto-size.php
@@ -0,0 +1,10 @@
+
+<style>
+    .box {border:1px solid #ccc; height:300px;}
+    .left {background: green; float:left; width: 300px;}
+    .right {background: blue; }
+</style>
+
+
+<div class="left box"></div>
+<div class="right box"></div>
---
+++ float-auto-size2.php
@@ -0,0 +1,13 @@
+
+<style>
+    .box {border:1px solid #ccc; height:300px;}
+    .left {background: green; float:left; width: 300px;}
+    .center {background: blue; }
+    .right {background: red;  float:right; width: 300px;}
+</style>
+
+
+<div class="left box"></div>
+<div class="right box"></div>
+<div class="right box"></div>
+<div class="center box"></div>
